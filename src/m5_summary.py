af3fae2eb5cb0aaf814051999904fe1ec92938c0
shepheam <shepheam@rose-hulman.edu>
2018-03-05 11:26:37 -0500

    I made my sister into a turtle :-)

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and ASHLEY_SHEPHERD.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -27,3 +27,23 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+brooke = rg.SimpleTurtle('turtle')
+brooke.speed = 20
+brooke.pen = rg.Pen('red',4)
+size = 150
+for k in range (7):
+    brooke.draw_circle(size)
+    brooke.pen_up()
+    brooke.right(45)
+    brooke.forward(20)
+    brooke.left(45)
+    brooke.pen_down()
+    size= size - 17
+
+window.close_on_mouse_click()
+
+
+
