cc5aa35b06222502aa0656effac7eb2e212d1b04
shepheam <shepheam@rose-hulman.edu>
2018-03-05 11:38:27 -0500

    I am a turtle and so is my sister.

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and ASHLEY_SHEPHERD.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
@@ -27,3 +27,34 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+window = rg.SimpleTurtle()
+
+ashley = rg.SimpleTurtle('turtle')
+ashley.speed = 75
+ashley.pen = rg.Pen('blue',5)
+
+size = 75
+for k in range (5):
+    ashley.draw_circle(size)
+    ashley.pen_up()
+    ashley.right(22)
+    ashley.backward(144)
+    ashley.left(22)
+    ashley.pen_down()
+    size = size - 12
+
+brooke = rg.SimpleTurtle('turtle')
+brooke.speed = 100
+brooke.pen = rg.Pen('pink',5)
+
+size = 125
+for k in range (20):
+    brooke.draw_square(size)
+    brooke.pen_up()
+    brooke.right(45)
+    brooke.forward(122)
+    brooke.left(45)
+    brooke.pen_down()
+    size = size - 30
